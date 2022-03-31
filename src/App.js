bc4fede9dd051c12aabe62783659353dbe3c259d
EmP33 <purwinmarek16@gmail.com>
2022-03-31 22:27:23 +0200

    Add some dumb stuff

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
-import { Redirect, Route, Switch } from "react-router-dom";
-import NewQuote from "./pages/NewQuote";
-import AllQuotes from "./pages/AllQuotes";
-import NotFound from "./pages/NotFound";
-import QuoteDetail from "./pages/QuoteDetail";
+import React, { Suspense } from "react";
 
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+import AllQuotes from "./pages/AllQuotes";
 import Layout from "./components/layout/Layout"; // Includes Navigation and main content
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
 
 function App() {
   return (
     <div>
       <Layout>
-        <Switch>
-          <Route path="/" exact>
-            <Redirect to="/quotes" />
-          </Route>
-          <Route path="/quotes" exact>
-            <AllQuotes />
-          </Route>
-          <Route path="/quotes/:quoteId">
-            <QuoteDetail />
-          </Route>
-          <Route path="/new-quote">
-            <NewQuote />
-          </Route>
-          <Route path="*">
-            <NotFound />
-          </Route>
-        </Switch>
+        <Suspense
+          fallback={
+            <div className="centered">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <Switch>
+            <Route path="/" exact>
+              <Redirect to="/quotes" />
+            </Route>
+            <Route path="/quotes" exact>
+              <AllQuotes />
+            </Route>
+            <Route path="/quotes/:quoteId">
+              <QuoteDetail />
+            </Route>
+            <Route path="/new-quote">
+              <NewQuote />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Suspense>
       </Layout>
     </div>
   );
